c2699f5d3622d84091952a8f2d62fc2232f74fed
Lionell Brown <ltrainb@gmail.com>
2014-06-26 10:41:11 -0400

    add code
    code added

---
+++ FirstCSharpCode/FirstCSharpCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCSharpCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("Hello World!");
+            input=Console.ReadLine();
+            Console.WriteLine(input);
+
+            
+        }
+    }
+}
